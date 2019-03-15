ef0046d6a236cb5a8672f5b4dc84dcc2eefc2607
Denys <deniskole1@gmail.com>
2019-03-15 20:44:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testemployees/pojo/Employee.java
@@ -0,0 +1,66 @@
+package com.example.testemployees.pojo;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Employee {
+
+    @SerializedName("f_name")
+    @Expose
+    private String name;
+    @SerializedName("l_name")
+    @Expose
+    private String lName;
+    @SerializedName("birthday")
+    @Expose
+    private String birthday;
+    @SerializedName("avatr_url")
+    @Expose
+    private String avatrUrl;
+    @SerializedName("specialty")
+    @Expose
+    private List<Specialty> specialty = null;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String fName) {
+        this.name = fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAvatrUrl() {
+        return avatrUrl;
+    }
+
+    public void setAvatrUrl(String avatrUrl) {
+        this.avatrUrl = avatrUrl;
+    }
+
+    public List<Specialty> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(List<Specialty> specialty) {
+        this.specialty = specialty;
+    }
+
+}
